632b3a65a86af32550b5942209f488e1bbdf5419
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2019-11-18 13:24:25 -0300

    improving and styling the modal bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,94 @@
 import 'package:flutter/material.dart';
-import './widgets/user_transactions.dart';
+import 'package:expenses/widgets/new_transaction.dart';
+import './widgets/transaction_list.dart';
+import 'models/transaction.dart';
 
 void main(){
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Home(),
   ));
 }
 
-class Home extends StatelessWidget {
-
+class Home extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Expenses'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: (){},
-          ),
-        ],
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+    final List<Transaction> _userTransactions = [
+      Transaction(
+        id: 't1',
+        title: 'New Shoes',
+        amount: 69.99,
+        date: DateTime.now(),
       ),
+      Transaction(
+          id: 't2',
+          title: 'Weekly Groceries',
+          amount: 35.67,
+          date: DateTime.now()
+      ),
+    ];
+
+    void _addNewTransaction(String txTitle, double txAmount){
+      final newTx = Transaction(
+          title: txTitle,
+          amount: txAmount,
+          date: DateTime.now(),
+          id: DateTime.now().toString()
+      );
+
+      setState(() {
+        _userTransactions.add(newTx);
+      });
+    }
+
+    void _startAddNewTransaction(BuildContext ctx){
+      showModalBottomSheet(
+        context: ctx,
+        builder: (_){
+          return GestureDetector(
+            onTap: (){},
+            behavior: HitTestBehavior.opaque,
+            child: NewTransaction(_addNewTransaction),
+          );
+        },
+      );
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Expenses'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () =>_startAddNewTransaction(context),
+            ),
+          ],
+        ),
         body: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  width: double.infinity,
-                  child: Card(
-                    child: Text('CHART'),
-                    elevation: 5,
-                  ),
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Card(
+                  child: Text('CHART'),
+                  elevation: 5,
                 ),
-                UserTransaction(),
-              ],
+              ),
+              TransactionList(_userTransactions),
+            ],
           ),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: (){},
+          onPressed: () =>_startAddNewTransaction(context),
         ),
-    );
+      );
+    }
   }
-}
--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
   final Function addTx;
-  final _ctlTitle = TextEditingController();
-  final _ctlAmount = TextEditingController();
 
   NewTransaction(this.addTx);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final _ctlTitle = TextEditingController();
+
+  final _ctlAmount = TextEditingController();
 
   void submitData(){
     final enteredTitle = _ctlTitle.text;
@@ -15,10 +22,12 @@ class NewTransaction extends StatelessWidget {
       return;
     }
 
-    addTx(
+    widget.addTx(
       _ctlTitle.text,
       double.parse(_ctlAmount.text)
     );
+
+    Navigator.of(context).pop();
   }
 
   @override
