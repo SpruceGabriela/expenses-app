cb5f876f08d136a975570dc22d0c51ab000cd481
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2019-11-25 14:21:55 -0300

    using cupertino widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:expenses/widgets/chart.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:expenses/widgets/new_transaction.dart';
 import './widgets/transaction_list.dart';
@@ -91,7 +93,19 @@ class _HomeState extends State<Home> {
     Widget build(BuildContext context) {
       final mediaQuery = MediaQuery.of(context);
       final isLandscape = mediaQuery.orientation == Orientation.landscape;
-      final appBar = AppBar(
+      final PreferredSizeWidget appBar = Platform.isIOS
+          ?  CupertinoNavigationBar(
+              middle: Text('Personal Expenses'),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                GestureDetector(
+                  child: Icon(CupertinoIcons.add),
+                  onTap: () => _startAddNewTransaction(context),
+                ),
+              ],),
+            )
+          : AppBar(
           title: Text('Personal Expenses'),
           actions: <Widget>[
             IconButton(
@@ -109,49 +123,64 @@ class _HomeState extends State<Home> {
         child: TransactionList(_userTransactions, _deleteTransaction)
         );
 
-      return Scaffold(
-        appBar: appBar,
-        body: SingleChildScrollView(
+      final pageBody = SafeArea(
+        child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               if(isLandscape) Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                Text('Show Chart'),
-                Switch(
-                  value: _showChart,
-                  onChanged: (val){
-                    setState(() {
-                      _showChart = val;
-                    });
-                  },)
-              ],),
+                  Text(
+                    'Show Chart',
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  Switch.adaptive(
+                    activeColor: Theme.of(context).accentColor,
+                    value: _showChart,
+                    onChanged: (val){
+                      setState(() {
+                        _showChart = val;
+                      });
+                    },)
+                ],),
               if(!isLandscape) Container(
-                height: (mediaQuery.size.height -
-                appBar.preferredSize.height -
-                mediaQuery.padding.top
-                ) * 0.3,
-                child: Chart(_userTransactions)
-                ),
+                  height: (mediaQuery.size.height -
+                      appBar.preferredSize.height -
+                      mediaQuery.padding.top
+                  ) * 0.3,
+                  child: Chart(_userTransactions)
+              ),
 
               if(!isLandscape) txListWidget,
 
               if(isLandscape) _showChart ? Container(
-                height: (mediaQuery.size.height -
-                appBar.preferredSize.height -
-                mediaQuery.padding.top
-                ) * 0.7,
-                child: Chart(_userTransactions)
+                  height: (mediaQuery.size.height -
+                      appBar.preferredSize.height -
+                      mediaQuery.padding.top
+                  ) * 0.7,
+                  child: Chart(_userTransactions)
               ) : txListWidget
             ],
           ),
         ),
+      );
+
+      return Platform.isIOS
+          ? CupertinoPageScaffold(
+        child: pageBody,
+        navigationBar: appBar,
+      )
+          : Scaffold(
+        appBar: appBar,
+        body: pageBody,
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () =>_startAddNewTransaction(context),
-        ),
+        floatingActionButton: Platform.isIOS
+            ? Container()
+            : FloatingActionButton(
+                child: Icon(Icons.add),
+                onPressed: () =>_startAddNewTransaction(context),
+              ),
       );
     }
   }
--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -90,7 +92,10 @@ class _NewTransactionState extends State<NewTransaction> {
                           : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
                       ),
                     ),
-                    FlatButton(
+                    Platform.isIOS ? CupertinoButton(
+                      child: Text('Choose Date', style: TextStyle(fontWeight: FontWeight.bold),),
+                      onPressed: _presentDatePicker,
+                    ) : FlatButton(
                       textColor: Theme.of(context).primaryColor,
                       child: Text('Choose Date', style: TextStyle(fontWeight: FontWeight.bold),),
                       onPressed: _presentDatePicker,
